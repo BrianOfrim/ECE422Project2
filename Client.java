6272d9fc0a5dd06d3dc414e3b0921d386c162a86
BrianOfrim <bofrim@ualberta.ca>
2017-04-06 20:43:09 -0600

    Some progress

--- Client.java
+++ Client.java
@@ -1,28 +1,102 @@
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
 	private static final Integer COMSPORT = 16000;
 	private static final String SERVERADDRESS = "localhost";
+	private static final String ACCESSGRANTED = "G";
+	private static final String ACCESSDENIED = "D";
+	private static final String SIGNUP = "U";
+	private static final String SIGNOUT = "I";
+	private static final String FILEFOUND = "F";
+	private static final String FILENOTFOUND = "N";
+			
+	
 	public static void main(String[] args) throws IOException {
 		Socket socket = new Socket(SERVERADDRESS, COMSPORT);
 		BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
 		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Signin (" + SIGNOUT + "), Signup (" + SIGNUP + ")");
 		String userInput;
-		System.out.println ("Type Message (\"Bye.\" to quit)");
-		while ((userInput = stdIn.readLine()) != null){
-			output.println(userInput);
+		userInput = stdIn.readLine();
+		
+		if(userInput.equals("U")){
+			output.println("U");
+			System.out.println("Username: ");
+			String userName =  stdIn.readLine();
+			System.out.println("Password: ");
+			String password =  stdIn.readLine();
+			if(userName.contains(" ")){
+				System.out.println("ERROR: username cannot contain spaces, exiting.");
+			}else{
+				output.println(userName);
+				output.println(password);
+				System.out.println("Account created please log back in to use it, exiting.");
+			}
 
-         // end loop
-         if (userInput.equals("Bye."))
-             break;
+			
+			
+		}else if(userInput.equals("I")){
+			output.println("I");
+			System.out.println("Username: ");
+			String userName =  stdIn.readLine();
+			output.println(userName);
+			System.out.println("Password: ");
+			String password =  stdIn.readLine();
+			output.println(password);
+			String credentialCheckMsg = input.readLine();
+			if(credentialCheckMsg.equals(ACCESSGRANTED)){
+				System.out.println ("Valid credentials, enter filenames (\"exit\" to quit)");
+				
+				while ((userInput = stdIn.readLine()) != null){
+					output.println(userInput); // send the name of the file
+					String fileFound = input.readLine();
+					
+					if(fileFound.equals(FILEFOUND)){
+						Integer lengthOfFile = Integer.parseInt(input.readLine());
+						// Receive the file 
+				        InputStream in = socket.getInputStream();
+				        System.out.println("Reciving file: " + userInput);
+				        OutputStream out = new FileOutputStream("./" + userInput);
+				        
+				        byte[] fileBytes = new byte[lengthOfFile];
+				        
+				        int byteCount;
+				        while((byteCount = in.read(fileBytes)) > 0){
+				        	out.write(fileBytes, 0, byteCount);
+				        }
+				        out.close();
+				        in.close();
+				        
+					}else if(fileFound.equals(FILENOTFOUND)){
+						System.out.println("Sorry file not found.");
+					}else{
+						System.out.println("Protocal error, exiting.");
+						break;
+					}
+					
+	
+		         // end loop
+		         if (userInput.equals("exit"))
+		             break;
+	
+			    System.out.println("echo: " + input.readLine());
+				}
+		   }else if(credentialCheckMsg.equals(ACCESSDENIED)){
+			   System.out.println("Invalid cedentials, exiting.");
+		   }else{
+			   System.out.println("Protocal error, exiting.");
+		   }
+		}
 
-	    System.out.println("echo: " + input.readLine());
-	   }
 		output.close();
 		input.close();
 		stdIn.close();
--- Server.java
+++ Server.java
@@ -1,13 +1,33 @@
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
 
 public class Server extends Thread{
 	private Socket socket;
 	private static final Integer COMSPORT = 16000;
+	private static final String SHADOWFILENAME = "shadow.txt";
+	private static final String ACCESSGRANTED = "G";
+	private static final String ACCESSDENIED = "D";
+	private static final String SIGNUP = "U";
+	private static final String SIGNOUT = "I";
+	private static final String FILEFOUND = "F";
+	private static final String FILENOTFOUND = "N";
+	
 	public static void main(String[] args) throws IOException {
 		 ServerSocket serverSocket = null; 
 		 serverSocket = new ServerSocket(COMSPORT);
@@ -20,19 +40,139 @@ public class Server extends Thread{
 		socket = s;
 		start();
 	}
+	
+	// copied from https://stackoverflow.com/questions/4895523/java-string-to-sha1
+	private static String byteArrayToHexString(byte[] b) {
+		  String result = "";
+		  for (int i=0; i < b.length; i++) {
+		    result +=
+		          Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+		  }
+		  return result;
+	}
+	
+	// modified from https://stackoverflow.com/questions/4895523/java-string-to-sha1
+	private static String hashPassword(byte[] pw){
+	    MessageDigest md = null;
+	    try {
+	        md = MessageDigest.getInstance("SHA-1");
+	    }
+	    catch(NoSuchAlgorithmException e) {
+	        e.printStackTrace();
+	    } 
+	    return byteArrayToHexString(md.digest(pw));
+		
+	}
+	
+	// some code copied from http://beginnersbook.com/2014/01/how-to-append-to-a-file-in-java/
+	private static Boolean writeToShadow(String userName, String hashedPasseord){
+		try{
+			
+			File file = new File(SHADOWFILENAME);
+	    	if(!file.exists()){
+	     	   file.createNewFile();
+	     	}	
+	    	FileWriter fw = new FileWriter(file,true);
+	    	BufferedWriter bw = new BufferedWriter(fw);
+	    	
+	    	bw.write(userName + " " + hashedPasseord + System.lineSeparator() );
+	    	bw.close();
+		}catch(IOException ioe){
+			return false;
+		}
+		return true;
+	}
+	
+	private static Boolean validateCredentials(String userName, String hashedPassword){
+		ArrayList<String> Lines = new ArrayList<String>();
+		try{
+			Files.lines(Paths.get("./" + SHADOWFILENAME)).forEach(line -> Lines.add(line));
+		}catch(IOException ioe){
+			return false;
+		}
+		for(String line: Lines){
+			String[] credentials = line.split(" ");
+			if(credentials.length == 2 && credentials[0].equals(userName) && credentials[1].equals(hashedPassword)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private static File retriveFile(String fileName){
+		File f = new File(fileName);
+		if(f.exists() && !f.isDirectory()) { 
+		    return f;
+		}
+		return null;
+	}
+	
+
+	
 	public void run(){
 		try{
 		PrintWriter output = new PrintWriter(socket.getOutputStream(), true); 
 		BufferedReader input = new BufferedReader(new InputStreamReader( socket.getInputStream()));
 		String inputLine;
 		
-        while ((inputLine = input.readLine()) != null) { 
-            System.out.println ("Server: " + inputLine); 
-            output.println(inputLine); 
+		if(input.readLine().equals(SIGNUP)){
+			System.out.println("User signing up");
+			
+			String userName = input.readLine();
+			String password = input.readLine();
+			String hashedPW = hashPassword((password + userName).getBytes());
+			
+			System.out.println("Username: " + userName);
+			System.out.println("Hashed password: " + hashedPW);
+			writeToShadow(userName,hashedPW);
+			
+		}else if(input.readLine().equals(SIGNOUT)){
+			System.out.println("User signing in");
+			
+			String userName = input.readLine();
+			String password = input.readLine();
+			String hashedPW = hashPassword((password + userName).getBytes());
+			
+			System.out.println("Username: " + userName);
+			System.out.println("Hashed password: " + hashedPW);
+			if(validateCredentials(userName, hashedPW)){
+				System.out.println("Valid credentials");
+				output.println(ACCESSGRANTED);
+		        while ((inputLine = input.readLine()) != null) { 
+		            System.out.println ("File requested: " + inputLine); 
+		            File file = retriveFile(inputLine);
+		            if(file != null){ // file exists sent it
+		            	output.println(FILEFOUND);
+		            	output.println(file.length()); // send the length of file
+		            	// write the file to the socket
+		            	byte[] fileByteArray = new byte [(int) file.length()];
+		            	FileInputStream fileInputStream = new FileInputStream(file);
+		            	OutputStream outputStream = socket.getOutputStream();
+		            	int numBytes;
+		            	while((numBytes = fileInputStream.read(fileByteArray)) > 0){
+		            		outputStream.write(fileByteArray,0,numBytes);
+		            	}
+		            	outputStream.close();
+		            	fileInputStream.close();
+		            	
+		            }else{ // file does not exits
+		            	output.println(FILENOTFOUND);
+		            	
+		            }
+		            output.println(inputLine); 
 
-            if (inputLine.equals("Bye.")) 
-                break; 
-        } 
+		            if (inputLine.equals("exit")) 
+		                break; 
+		        } 
+			}else{
+				System.out.println("Invalid credentials");
+				output.println(ACCESSDENIED);
+			}
+
+		}else{
+			System.out.println("Procal Error");
+		}
+		
         output.close(); 
         input.close(); 
         socket.close();

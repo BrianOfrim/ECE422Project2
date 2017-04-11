591193b390ff6296ce90b971b2b081c1f6f3673e
BrianOfrim <bofrim@ualberta.ca>
2017-04-10 20:28:48 -0600

    fixed bug in getting byte arrays jni

--- lib_encrypt.c
+++ lib_encrypt.c
@@ -17,13 +17,13 @@ JNIEXPORT jbyteArray JNICALL Java_Encryption_encrypt
 	lenData = (*env)->GetArrayLength(env, data);
 	lenKey = (*env)->GetArrayLength(env, key);
 
-	myCopyData = (jbyte *) (*env)->GetIntArrayElements(env, data, is_copy_data);
+	myCopyData = (jbyte *) (*env)->GetByteArrayElements(env, data, is_copy_data);
     if (is_copy_data == NULL){
         printf("Cannot obtain data array from JVM\n");
         exit(0);
     }
 
-	myCopyKeys = (jbyte *) (*env)->GetIntArrayElements(env, key, is_copy_key);
+	myCopyKeys = (jbyte *) (*env)->GetByteArrayElements(env, key, is_copy_key);
     if (is_copy_key == NULL){
         printf("Cannot obtain key array from JVM\n");
         exit(0);
